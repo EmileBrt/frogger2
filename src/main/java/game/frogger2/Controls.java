9e2737bcfbd7803b92151681add3bcd89e0816e9
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2021-11-01 12:55:56 +0100

    ajouté énumération des touches de controle de la grenouille

---
+++ src/main/java/game/frogger2/Controls.java
@@ -0,0 +1,13 @@
+package game.frogger2;
+import util.ControlKeys;
+
+
+/**
+ * This class is used to control the movements of the frog.
+ */
+public class Controls {
+    private ControlKeys keyset;
+
+
+
+}
---
+++ src/main/java/util/ControlKeys.java
@@ -0,0 +1,12 @@
+package util;
+
+/**
+ * This enumeration lists the different key sets used to control the frog.
+ * @author Corentin GOETGHEBEUR
+ * @version 1.0
+ * @see game.frogger2.Controls
+ */
+public enum ControlKeys {
+    ZQSD,
+    Arrows
+}
