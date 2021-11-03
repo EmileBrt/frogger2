01fab52528aab231a1ebee322c935f241afef2ed
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2021-11-03 20:47:56 +0100

    bouton help connecté

--- src/main/java/game/frogger2/Menu.java
+++ src/main/java/game/frogger2/Menu.java
@@ -1,6 +1,7 @@
 package game.frogger2;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -20,7 +21,7 @@ public class Menu {
 
     private VBox menu_vbox;
     private Label title_label;
-    private Label descritption_label;
+    private Label description_label;
     private HBox button_box;
     private Button play_button, help_button, quit_button;
 
@@ -32,26 +33,34 @@ public class Menu {
 
     private void SceneSetup() throws IOException {
 
+        // vbox setup
         menu_vbox = new VBox();
         menu_vbox.setPrefHeight(400);
         menu_vbox.setPrefWidth(600);
         menu_vbox.setSpacing(20);
         menu_vbox.setAlignment(Pos.CENTER);
+        Insets menu_inset = new Insets(0, 20, 0, 20);
+        menu_vbox.setPadding(menu_inset);
 
+        //title setup
         title_label = new Label();
         title_label.setText("Frogger");
         Font title_font = new Font("Impact", 70);
         title_label.setFont(title_font);
 
-        descritption_label = new Label();
-        descritption_label.setText("Press play to start !");
+        //description setup
+        description_label = new Label();
+        description_label.setText("Press play to start !");
         Font description_font = new Font(16);
-        descritption_label.setFont(description_font);
+        description_label.setFont(description_font);
+        description_label.setWrapText(true);
 
+        //button hbox setup
         button_box = new HBox();
         button_box.setAlignment(Pos.CENTER);
         button_box.setSpacing(50);
 
+        //buttons setup
         play_button = new Button();
         play_button.setText("PLAY");
 
@@ -63,7 +72,7 @@ public class Menu {
 
 
         button_box.getChildren().addAll(quit_button, help_button, play_button);
-        menu_vbox.getChildren().addAll(title_label, descritption_label, button_box);
+        menu_vbox.getChildren().addAll(title_label, description_label, button_box);
 
         menu_scene = new Scene(menu_vbox);
 
@@ -78,6 +87,7 @@ public class Menu {
         this.app = exec;
 
         play_button.setOnAction(actionEvent -> onPlayButtonClick());
+        help_button.setOnAction(actionEvent -> onHelpButtonClick());
         quit_button.setOnAction(actionEvent -> {
             try {
                 onQuitButtonClick();
@@ -102,5 +112,14 @@ public class Menu {
     public void onQuitButtonClick() throws Exception {
         window.close();
         app.stop();
+    }
+
+    /**
+     * Méthode appelée en cas d'appui sur le bouton Help.
+     */
+    public void onHelpButtonClick(){
+        description_label.setText("After pressing play, you can move the frog using ZQSD on your keyboard. " +
+                "(This version is made for AZERTY keyboards).\n" +
+                "Try not to get hit by the cars !");
     }
 }
